84ee3000b9088d91905c2cd849c3b128ffb528ea
Anand <maanand2k@gmail.com>
2024-03-26 13:32:26 +0530

    Changes in xmlreader file and sample xml file

--- XmlReader.java
+++ XmlReader.java
@@ -7,26 +7,38 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import org.w3c.dom.Element;
+
 public class XmlReader {
 
     public static void main(String[] args) throws Exception{
         
-        File xmlFile = new File("/home/madan/anand/Java Class Files/resources/sample2.xml");
+        File xmlFile = new File("/home/madan/anand/Java Class Files/resources/SBI_NTPC_20240303_04032024073001.xml");
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
         DocumentBuilder builder = factory.newDocumentBuilder();
 
         Document document = builder.parse(xmlFile);
+        document.getDocumentElement().normalize();
 
-        NodeList nodeList = document.getElementsByTagName("root");
+        NodeList nodeList = document.getElementsByTagName("TRANSACTION");
 
-        for(int i =0;i<nodeList.getLength();i++){
-            Node node = nodeList.item(i);
-            System.out.println(node);
+        for(int temp =0;temp<nodeList.getLength();temp++){
+            Node node = nodeList.item(temp);
+            // System.out.println("\nCurrent Element :" + node.getNodeName());
+if(node.getNodeType() == Node.ELEMENT_NODE){
+Element eElement = (Element) node;
+String refNo = eElement.getElementsByTagName("REFNO").item(0).getTextContent();
+String paymentMode = eElement.getElementsByTagName("PAYMODE").item(0).getTextContent();
+String utrNo = eElement.getElementsByTagName("UTRNO").item(0).getTextContent();
+String totalAmount = eElement.getElementsByTagName("TOTALAMOUNT").item(0).getTextContent();
+System.out.println("Ref No : "+refNo);
+System.out.println("UTR No : "+utrNo);
+System.out.println("Paytm :"+paymentMode);
+System.out.println("Total Amount :"+totalAmount);
+}
         }
-
-        System.out.println(nodeList);
 
     }
     
