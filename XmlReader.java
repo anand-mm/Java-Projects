271bf70d1672f039fda1a74e35b71e81ee9234ee
Anand <maanand2k@gmail.com>
2024-04-01 18:52:07 +0530

    Updated xml parsing

--- XmlReader.java
+++ XmlReader.java
@@ -4,16 +4,13 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
-import org.w3c.dom.Element;
 
 public class XmlReader {
 
     public static void main(String[] args) throws Exception{
         
-        File xmlFile = new File("/home/madan/anand/Java Class Files/resources/SBI_NTPC_20240303_04032024073001.xml");
+        File xmlFile = new File("/home/madan/anand/Java Class Files/resources/SBI_UP_20240302_03032024073001.xml");
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
@@ -22,24 +19,24 @@ public class XmlReader {
         Document document = builder.parse(xmlFile);
         document.getDocumentElement().normalize();
 
-        NodeList nodeList = document.getElementsByTagName("TRANSACTION");
-
-        for(int temp =0;temp<nodeList.getLength();temp++){
-            Node node = nodeList.item(temp);
-            // System.out.println("\nCurrent Element :" + node.getNodeName());
-if(node.getNodeType() == Node.ELEMENT_NODE){
-Element eElement = (Element) node;
-String refNo = eElement.getElementsByTagName("REFNO").item(0).getTextContent();
-String paymentMode = eElement.getElementsByTagName("PAYMODE").item(0).getTextContent();
-String utrNo = eElement.getElementsByTagName("UTRNO").item(0).getTextContent();
-String totalAmount = eElement.getElementsByTagName("TOTALAMOUNT").item(0).getTextContent();
-System.out.println("Ref No : "+refNo);
-System.out.println("UTR No : "+utrNo);
-System.out.println("Paytm :"+paymentMode);
-System.out.println("Total Amount :"+totalAmount);
-}
-        }
+        traverseNode(document);
 
     }
-    
+
+    public static void traverseNode(Document document){
+        NodeList first = document.getElementsByTagName("TRANSACTION");
+    //     for(int temp =0;temp<first.getLength();temp++){
+    //         Node getNodeList = first.item(temp);
+    //         NodeList getChildLength = getNodeList.getChildNodes(); 
+    //     Node currentNode;
+    //     for(int i =0;i<getChildLength.getLength();i++){
+    //         currentNode = getChildLength.item(i);
+    //         if(currentNode.getNodeType()==Node.ELEMENT_NODE)
+    //         System.out.println(currentNode.getNodeName() +" : " +currentNode.getTextContent());
+    //     }
+    //     System.out.println();
+    // }
+    System.out.println("Total Transaction From the XML :" + first.getLength());
+}
+
 }
